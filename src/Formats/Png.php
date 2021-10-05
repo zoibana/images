f16286edf01772d157c41c2156181aaf1d8f8852
Zoibana <zoibana@gmail.com>
2021-10-05 16:12:51 +0300

    Added transparency support to PNG

--- src/Formats/Png.php
+++ src/Formats/Png.php
@@ -9,10 +9,14 @@ class Png extends ImageResource
 	public function fromFile(string $src_path): void
 	{
 		$this->resource = imagecreatefrompng($src_path);
+
+		imagealphablending($this->resource, false);
+		imagesavealpha($this->resource, true);
 	}
 
 	public function save(string $dest_path = null, $quality = null): bool
 	{
+		imageinterlace($this->resource, false);
 		return imagepng($this->resource, $dest_path, $quality);
 	}
 
